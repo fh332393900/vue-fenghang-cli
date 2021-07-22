abb7f90e42deb1bc06ffa76e603206cc38f6fb85
fenghang <332393900@qq.com>
2021-07-22 17:48:49 +0800

    添加

---
+++ bin/cli.js
@@ -0,0 +1,20 @@
+#! /usr/bin/env node
+const program = require('commander')
+
+program
+  // 定义命令和参数
+  .command('create <app-name>')
+  .description('create a new project')
+  // -f or --force 为强制创建，如果创建的目录存在则直接覆盖
+  .option('-f, --force', 'overwrite target directory if it exist')
+  .action((name, options) => {
+    require('../lib/create.js')(name, options)
+  })
+  
+program
+   // 配置版本号信息
+  .version(`v${require('../package.json').version}`)
+  .usage('<command> [option]')
+  
+// 解析用户执行命令传入参数
+program.parse(process.argv);
